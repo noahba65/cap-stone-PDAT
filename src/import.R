fd35154ff6057625807ad12374c448ac09436977
Noah Anderson <noahanderson6556@gmail.com>
2023-10-29 17:28:10 -0600

    Finish first draft

--- src/import.R
+++ src/import.R
@@ -1,23 +1,31 @@
+# Base URL for fetching Divvy bike share data from the City of Chicago's database
+divvy_url <- "https://data.cityofchicago.org/resource/fg6s-gzvg.json?"
 
-# Construct URL
+# Construct the full URL for data retrieval
+# Checks if 'limit' is null, and constructs the URL accordingly
 divvy_url_full <- ifelse( is.null(limit),
-paste0(divvy_url,"$where=start_time >= '", start_date, 
-       "' AND start_time < '", end_date, 
-       "'&$order=", order_by),
-paste0(divvy_url,"$where=start_time >= '", start_date, 
-       "' AND start_time < '", end_date, 
-       "'&$order=", order_by, 
-       "&$limit=", limit)
+                          # Construct URL without limit parameter
+                          paste0(divvy_url,"$where=start_time >= '", start_date, 
+                                 "' AND start_time < '", end_date, 
+                                 "'&$order=", order_by),
+                          # Construct URL with limit parameter
+                          paste0(divvy_url,"$where=start_time >= '", start_date, 
+                                 "' AND start_time < '", end_date, 
+                                 "'&$order=", order_by, 
+                                 "&$limit=", limit)
 )
 
+# Fetch Divvy data using the constructed URL and store in a dataframe
 divvy_df <- read.socrata(divvy_url_full)
 
-
-
+# Base URL for fetching rail stops data from the City of Chicago's database
 rail_stops_url <- "https://data.cityofchicago.org/resource/8pix-ypme.json"
 
+# Fetch rail stops data using the provided URL
 rail_stops <- read.socrata(rail_stops_url)
 
+# Retrieve Cook County's 2010 decennial population data at the tract level
+# Using the 'tidycensus' package
 cook_population <- get_decennial(geography = "tract", 
                                  variables = "P001001", 
                                  year = 2010, 
@@ -25,6 +33,8 @@ cook_population <- get_decennial(geography = "tract",
                                  county = "Cook County", 
                                  geometry = TRUE)
 
+# Retrieve weather data for ORD station between given start and end dates
+# Filters specific columns (valid, tmpf, p01i, sknt) from the fetched data
 weather.Data <- 
   riem_measures(station = "ORD", date_start = start_date, date_end = end_date) %>%
-  select(valid, tmpf, p01i, sknt) 
+  select(valid, tmpf, p01i, sknt)
--- src/main.R
+++ src/main.R
@@ -7,7 +7,6 @@ library(riem)
 
 
 # Define parameters
-divvy_url <- "https://data.cityofchicago.org/resource/fg6s-gzvg.json?"
 start_date <- "2019-01-01"
 end_date <- "2020-01-01"
 order_by <- "start_time DESC"
@@ -17,55 +16,3 @@ limit <- 10000
 
 source("src/import.R")
 source("src/cleaning.R")
-
-# divvy_url <- "https://data.cityofchicago.org/resource/fg6s-gzvg.json?$where=start_time >= '2019-01-01' AND start_time < '2020-01-01'&$order=start_time DESC&$limit=10000"
-# divvy_df <- read.socrata(divvy_url)
-# divvy_df$date_from <- as.Date(divvy_df$start_time)
-# divvy_df$date_to <- as.Date(divvy_df$stop_time)
-# 
-# from_trips_daily <- divvy_df %>%
-#   group_by(date_from, from_station_id, from_latitude, from_longitude) %>%
-#   summarize(daily_from_trips = n(), .groups = "drop")
-# to_trips_daily <- divvy_df %>%
-#   group_by(date_to, to_station_id, to_latitude, to_longitude) %>%
-#   summarize(daily_to_trips = n(), .groups = "drop")
-# daily_trips <- left_join(from_trips_daily, to_trips_daily, 
-#                          by = c("date_from" = "date_to", 
-#                                 "from_station_id" = "to_station_id")) %>%
-#   transmute(
-#     date = date_from,
-#     station_id = from_station_id,
-#     latitude = from_latitude,
-#     longitude = from_longitude,
-#     daily_from_trips = replace_na(daily_from_trips, 0),
-#     daily_to_trips = replace_na(daily_to_trips, 0),
-#     net_trips = daily_from_trips - daily_to_trips
-#   ) %>%
-#   st_as_sf(coords = c("longitude", "latitude"), crs = 4269)
-
-# cook_population <- get_decennial(geography = "tract", 
-#                                  variables = "P001001", 
-#                                  year = 2010, 
-#                                  state = "IL", 
-#                                  county = "Cook County", 
-#                                  geometry = TRUE)
-# 
-# daily_trips_pop <-  st_join(daily_trips, cook_population, join = st_within) %>%
-#   select(-c(NAME, variable, GEOID)) %>%
-#   rename(Population = value)
-# 
-# weather.Data <- 
-#   riem_measures(station = "ORD", date_start = as.character(min(daily_trips$date)), date_end = as.character(max(daily_trips$date))) %>%
-#   select(valid, tmpf, p01i, sknt) 
-# 
-# weather.Panel <-  
-#   weather.Data %>%
-#   mutate(date = as.Date(valid) ) %>%
-#   group_by(date) %>%
-#   summarize(HighTemp = max(tmpf, na.rm = T),
-#               LowTemp = min(tmpf, na.rm = T),
-#               Percipitation = sum(p01i, na.rm = T),
-#               Wind_Speed = max(sknt, na.rm = T))
-# 
-# daily_trips_full <- left_join(daily_trips_pop, weather.Panel)
-# head(daily_trips_full)
