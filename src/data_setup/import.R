de7ca26bd2d6f4806ebc66f044180c92aea956ae
Noah Anderson <noahanderson6556@gmail.com>
2023-12-01 13:49:50 -0600

    Fixed redundancy in import

--- src/data_setup/import.R
+++ src/data_setup/import.R
@@ -1,8 +1,7 @@
 chicago_ca_raw <- st_read("/Users/noahanderson/Documents/Data/Boundaries-Neighborhoods.geojson") 
 
 divvy_stations_url <- "https://data.cityofchicago.org/resource/bk89-9dk7.json"
-divvy_stations_raw <- read.socrata(divvy_stations_url) %>%
-  st_as_sf(coords = c("longitude", "latitude"), crs = 4269)
+divvy_stations_raw <- read.socrata(divvy_stations_url) 
 
 
 divvy_raw <- read_csv("/Users/noahanderson/Documents/Data/Divvy_Bicycle_Stations_-_Historical.csv")
